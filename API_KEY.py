892d955ca1bb496373b4cf0bc35f805a1a9e9a10
navither <87735806+navither@users.noreply.github.com>
2023-06-04 22:31:32 +0800

    Update API_KEY.py

--- API_KEY.py
+++ API_KEY.py
@@ -1,10 +1,1 @@
-import os
 
-city_location = "101230101"
-openai_key = "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"
-weather_key = "e2774b7beb8a4bdea88b24d089f95360"
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-vits_model_path = os.path.join(script_dir, 'model', 'tts', 'G_latest.pth')
-vits_config_path = os.path.join(
-    script_dir, 'model', 'tts', 'config.json')
